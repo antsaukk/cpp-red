708c960485f2a09af67253196c103d9887b2f274
Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
2021-09-12 22:07:11 +0300

    text editor finished

--- week_4/text_editor.cpp
+++ week_4/text_editor.cpp
@@ -1,18 +1,79 @@
 #include <string>
+#include <deque>
+#include <list>
+#include <cmath>
 #include "test_runner.h"
+#include "profile.h"
 using namespace std;
 
 class Editor {
  public:
   // Реализуйте конструктор по умолчанию и объявленные методы
-  Editor();
-  void Left();
-  void Right();
-  void Insert(char token);
-  void Cut(size_t tokens = 1);
-  void Copy(size_t tokens = 1);
-  void Paste();
-  string GetText() const;
+  Editor() {}
+
+  void Left() {
+    if (position != editor_state.begin()) { position = prev(position); }
+  }
+  void Right() {
+    if (position != editor_state.end()) { position = next(position); }
+  }
+
+  void Insert(char token) {
+    if (position == editor_state.end()) {
+      editor_state.push_back(token);
+      position = editor_state.end();
+    } else {
+      editor_state.insert(position, token);
+    }
+  }
+
+  void Cut(size_t tokens = 1) {
+    size_t count = 0; 
+    while(count < tokens && position != editor_state.end()) {
+      buffer.push_back(*position); 
+      auto temp = position; 
+      position = next(position);
+      editor_state.erase(temp);
+      count++;
+    }
+  }
+  void Copy(size_t tokens = 1) {
+    size_t count = 0;
+    while(count < tokens && position != editor_state.end()) {
+      buffer.push_back(*position); 
+      position = next(position);
+      count++;
+    }
+  }
+
+  void Paste() {
+    size_t count = 0; 
+    while (count < buffer.size()) {
+      char ch = buffer.front(); 
+      buffer.pop_front();
+      Insert(ch);
+      buffer.push_back(ch);
+      count++;
+    }
+  }
+
+  string GetText() const {
+    string s;
+    s.reserve(Size());
+    for(const auto& ch : editor_state) {
+      s+=ch;
+    }
+    return s;
+  }
+
+  size_t Size() const { return editor_state.size(); }
+
+  list<char>::iterator GetPos() { return position; }
+
+private:
+  list<char> editor_state;
+  list<char>::iterator position = editor_state.begin();
+  deque<char> buffer;
 };
 
 void TypeText(Editor& editor, const string& text) {
@@ -27,14 +88,14 @@ void TestEditing() {
 
     const size_t text_len = 12;
     const size_t first_part_len = 7;
-    TypeText(editor, "hello, world");
+    TypeText(editor, "hello, world"); 
     for(size_t i = 0; i < text_len; ++i) {
       editor.Left();
     }
-    editor.Cut(first_part_len);
+    editor.Cut(first_part_len); 
     for(size_t i = 0; i < text_len - first_part_len; ++i) {
       editor.Right();
-    }
+    } 
     TypeText(editor, ", ");
     editor.Paste();
     editor.Left();
@@ -46,12 +107,12 @@ void TestEditing() {
   {
     Editor editor;
     
-    TypeText(editor, "misprnit");
+    TypeText(editor, "misprnit"); 
     editor.Left();
     editor.Left();
-    editor.Left();
-    editor.Cut(1);
-    editor.Right();
+    editor.Left(); 
+    editor.Cut(1); 
+    editor.Right();  
     editor.Paste();
     
     ASSERT_EQUAL(editor.GetText(), "misprint");
@@ -104,11 +165,26 @@ void TestEmptyBuffer() {
   ASSERT_EQUAL(editor.GetText(), "example");
 }
 
+void BufferNoClean()
+{
+  Editor editor;
+  TypeText(editor, "1234567");
+  editor.Left();
+  editor.Left();
+  editor.Cut(1);
+  editor.Paste();
+  editor.Paste();
+  editor.Paste();
+  ASSERT_EQUAL(editor.GetText(), "123456667");
+}
+
 int main() {
   TestRunner tr;
+  LOG_DURATION("Total tests duration");
   RUN_TEST(tr, TestEditing);
   RUN_TEST(tr, TestReverse);
   RUN_TEST(tr, TestNoText);
   RUN_TEST(tr, TestEmptyBuffer);
+  RUN_TEST(tr, BufferNoClean);
   return 0;
 }
